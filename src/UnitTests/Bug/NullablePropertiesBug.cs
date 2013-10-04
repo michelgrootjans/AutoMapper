83129ef8f19c4bfc47db45e25bc1bbc8064c609b
Michel Grootjans <michel.grootjans@gmail.com>
2013-10-04 17:06:46 +0200

    reproduced bug

--- src/UnitTests/Bug/NullablePropertiesBug.cs
+++ src/UnitTests/Bug/NullablePropertiesBug.cs
@@ -18,4 +18,28 @@ namespace AutoMapper.UnitTests.Bug
             d.A.ShouldBeNull();
         }
     }
+
+    public class Int_to_NullableDecimal_Bug
+    {
+        public class Source { public int A { get; set; } }
+        public class Target { public decimal? A { get; set; } }
+
+        public Int_to_NullableDecimal_Bug()
+        {
+            Mapper.CreateMap<Source, Target>();
+        }
+
+        [Fact]
+        public void ConfigurationIsValid()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void MapsCorrectly()
+        {
+            var target = Mapper.Map<Source, Target>(new Source { A = 25 });
+            target.A.ShouldEqual(25.0m);
+        }
+    }
 }
